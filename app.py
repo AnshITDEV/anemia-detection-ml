11d81152117aebc4538658e0f79d08b2db68baf3
AnshITDEV <ansh.panchal@somaiya.edu>
2026-04-13 14:26:23 +0530

    deployment errors

--- app.py
+++ app.py
@@ -170,6 +170,6 @@ def predict():
             'error': str(e)
         }), 400
 
-if __name__ == "__main__":
-    import os
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+#if __name__ == "__main__":
+    #import os
+#app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
